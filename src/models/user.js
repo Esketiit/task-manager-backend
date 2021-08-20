be1784d00cf5ca5769ae9f89940f3b658b791cd5
github username <heftygac@gmail.com>
2021-08-20 11:16:36 -0400

    added user middleware

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+const Task = require('./task')
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -102,6 +103,13 @@ userSchema.pre('save', async function (next) {
   next()
 })
 
+// Delete user tasks when a user is removed
+userSchema.pre('delete', async function (next) {
+  const user = this
+  await Task.deleteMany({owner: user._id})
+  
+  next()
+})
 const User = mongoose.model('User', userSchema)
 
 module.exports = User
